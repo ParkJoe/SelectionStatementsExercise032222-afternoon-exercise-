100ab07d1f6e490fd1797ce205365516d8acbffb
Joe Park <joe_park@live.com>
2022-03-23 12:25:09 -0400

    More revisions to exercises 1 & 2

--- SelectionStatementsExercise032222(afternoon exercise)/Program.cs
+++ SelectionStatementsExercise032222(afternoon exercise)/Program.cs
@@ -8,61 +8,55 @@ namespace SelectionStatementsExercise032222_afternoon_exercise_
         {
             //Exercise # 1
 
-            Console.WriteLine("Welcome to the number guessing game!!");
-            Console.WriteLine("See if you can guess the correct number");
-            Console.WriteLine("What is the upper limit for the range of the number");
+            //var r = new Random();
+            //var favNumber = r.Next(1, 10);
 
-            var userResponse = Console.ReadLine();
+            //Console.WriteLine("Pick a number from 1 - 1000");
+            //var userInput = int.Parse(Console.ReadLine());
 
-            var upperLimit = int.Parse(userResponse);
+            //if (userInput < favNumber)
+            //{
+            //    Console.WriteLine($"{userInput} is too low");
+            //}
+            //else if (userInput > favNumber)
+            //{
+            //    Console.WriteLine($"{userInput} is too high");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You got it!!!");
+            //}
 
-            var random = new Random();
-            var number = random.Next(1, 10);
-
-            Console.WriteLine("Input your guess:");
-
-            var guess = int.Parse(Console.ReadLine());
-
-            if (guess < number)
-            {
-                Console.WriteLine("Your guess is too high, sorry!");
-            }
-            else if (guess < number)
-            {
-                Console.WriteLine("Your guess is too low, sorry!");
-            }
-            else
-            {
-                Console.Write("Correct, you got it!!!");
 
             //Exercise # 2
 
-                Console.WriteLine("\nWhat is your favorite school subject?");
+            Console.WriteLine("\nWhat is your favorite school subject?");
 
-                var subject = Console.ReadLine(); //Evaluate to a string
+            var subject = Console.ReadLine(); //Evaluate to a string
 
-                switch (subject.ToLower())
-                {
-                    case "c#":
-                        Console.WriteLine("C# is a very intense subject.");
-                        break;
-                    case "chemistry":
-                        Console.WriteLine("Chemistry is a tough subject.");
-                        break;
-                    case "calculus":
-                        Console.WriteLine("Calculus is a very difficult subject.");
-                        break;
-                    case "english":
-                        Console.WriteLine("English requires too much writing with essays and reports.");
-                        break;
-                    case "history":
-                        Console.WriteLine("History is interesting.");
-                        break;
-                    default:
-                        Console.WriteLine($"Yeah, I know what you mean! {subject} sounds difficult but cool too.");
-                        break;
+            switch (subject.ToLower())
+            {
+                case "c#":
+                case "programming":
+                    Console.WriteLine("C# is a very intense subject.");
+                    break;
+                case "chemistry":
+                case "science":
+                    Console.WriteLine("Chemistry is a tough subject.");
+                    break;
+                case "calculus":
+                    Console.WriteLine("Calculus is a very difficult subject.");
+                    break;
+                case "english":
+                    Console.WriteLine("English requires too much writing with essays and reports.");
+                    break;
+                case "history":
+                    Console.WriteLine("History is interesting.");
+                    break;
+                default:
+                    Console.WriteLine($"Yeah, I know what you mean! {subject} sounds difficult but cool too.");
+                    break;
 
-                }
 
             }
 
@@ -73,7 +67,13 @@ namespace SelectionStatementsExercise032222_afternoon_exercise_
 }
 
 
-       
+
+
+
+
+
+
+
 
 
 
